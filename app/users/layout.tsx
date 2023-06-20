d9f24f2ddf576b0b776f88a645b545d36c953762
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-06-20 10:55:27 +0800

    add: userlist UI

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,20 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
   return (
     // * maybe use "@ts-expected-error Server Component" if there a typescript error
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 }
