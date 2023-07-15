edced6c135859692bb0a5576077d1013439747ff
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-07-15 18:58:39 +0800

    add: loading modal

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -30,9 +31,11 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   }, [data, router]);
 
   return (
-    <div
-      onClick={handleClick}
-      className="
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="
         w-full
         relative
         flex
@@ -45,31 +48,32 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
         transition
         cursor-pointer
     "
-    >
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-non">
-          <div
-            className="
+      >
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-non">
+            <div
+              className="
                 flex
                 justify-between
                 items-center
                 mb-1
             "
-          >
-            <p
-              className="
+            >
+              <p
+                className="
                 text-sm
                 font-medium
                 text-gray-900
               "
-            >
-              {data.name}
-            </p>
+              >
+                {data.name}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
